c2d63902474ab2ae6a8012dd6f0ebc4d5fd54d0a
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-07 00:57:48 -0400

    Rendered All Students View

---
+++ src/components/views/AllStudentsView.js
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const AllStudentsView = (props) => {
+  if (!props.allStudents.length) {
+    return <div>There are no students.</div>;
+  }
+
+  return (
+    <div>
+      {props.allStudents.map((student) => (
+        <div key={student.id}>
+          <Link to={`/student/${student.id}`}>
+            <h1>{`${student.firstname} ${student.lastname}`}</h1>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+AllStudentsView.propTypes = {
+  allStudents: PropTypes.array.isRequired,
+};
+
+export default AllStudentsView;
