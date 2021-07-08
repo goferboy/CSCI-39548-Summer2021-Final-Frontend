3a47292a1682a0f571c7ed587cddbf7e8be666d9
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-08 03:44:33 -0400

    Styled Student View

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -2,17 +2,47 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { EditStudentContainer } from '../containers';
+import "../css/StudentView.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: "15px"
+  }
+}));
 
 const StudentView = (props) => {
-    const {student} = props;
+  const classes = useStyles();
+  const {student} = props;
     return (
-      <div>      
-        <h1>{student.firstname + " " + student.lastname}</h1>
-        <p>{
-            student.campusId
-            ? `${student.campus.name}`
-            : "Not attending any school"
-        }</p>
+      <div id="student-view">
+        {
+          student.imageURL === "https://cdn.onlinewebfonts.com/svg/img_210318.png"
+          ? <img id="no-student-img" src={student.imageURL} />
+          : <img className="student-img" src={student.imageURL} />
+        }
+        <div className="student-view-text">      
+          <h1>{student.firstname + " " + student.lastname}</h1>
+          <h2>{student.email}</h2>
+          <h3>{
+              student.campusId
+              ? `Attending: ${student.campus.name}`
+              : "Not attending any school"
+          }</h3>
+           <h3>GPA: {
+              student.gpa
+              ? student.gpa
+              : "N/A"
+          }</h3>
+        </div>
+        <div className="button-menu">
+          <Button className={classes.root} onClick={props.showEditClick} variant="contained" 
+            color={props.showEdit ? "secondary" : "primary"}>
+            Edit Student
+          </Button>
+          <Button className={classes.root} onClick={props.handleClick} variant="contained" color="primary">
+            Delete Student
+          </Button>
+        </div>
         <div className="edit-form">
           {
             props.showEdit
@@ -20,12 +50,6 @@ const StudentView = (props) => {
             : <></>
           }
         </div>
-        <Button onClick={props.showEditClick} variant="contained" color="primary">
-          Edit Student
-        </Button>
-        <Button onClick={props.handleClick} variant="contained" color="primary">
-          Delete Student
-        </Button>
       </div>
     );
   };
