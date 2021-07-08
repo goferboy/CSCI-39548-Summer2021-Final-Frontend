61290fd70282169b9e0fa4306e23df0b9b70cc59
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-08 03:07:24 -0400

    Styled Add Campus View

--- src/components/views/AddCampusView.js
+++ src/components/views/AddCampusView.js
@@ -1,21 +1,40 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import "../css/AddCampusView.css";
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: "15px"
+  }
+}));
 
 const AddCampusView = (props) => {
+  const classes = useStyles();
   return (
-    <div>
-        <h1>add Campus</h1>
-        <form onSubmit={props.handleSubmit}>
-            <label htmlFor="name">Name: </label>
-            <input type="text" name="name" placeholder="Name" required />
-            <label htmlFor="address">Address: </label>
-            <input type="text" name="address" placeholder="Address" required />
-            <label htmlFor="imageURL">Image URL: </label>
-            <input type="text" name="imageURL" placeholder="http://" />
-            <label htmlFor="description">Description: </label>
-            <input type="text" name="description" placeholder="Description" />
-            <Button type="submit" className="edit-submit" variant="contained" color="primary">Submit</Button>
+    <div id="add-campus">
+        <h1>Add Campus</h1>
+        <form id="add-campus-form" onSubmit={props.handleSubmit}>
+          <TextField 
+              className={classes.root} 
+              variant="outlined" label="Name" name="name" 
+              fullWidth required/>
+          <TextField 
+              className={classes.root} 
+              variant="outlined" label="Address" name="address" 
+              fullWidth required/>
+          <TextField 
+              className={classes.root} 
+              variant="outlined" label="Image URL" name="imageURL"
+              fullWidth />
+          <TextField
+              className={classes.root}
+              variant="outlined" label="Description" name="description"
+              fullWidth/>
+          <Button 
+              className={classes.root} type="submit"
+              variant="contained" color="primary">Submit</Button>
         </form>
     </div>
   );
--- src/components/views/EditCampusView.js
+++ src/components/views/EditCampusView.js
@@ -1,6 +1,5 @@
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import "../css/EditCampusView.css";
