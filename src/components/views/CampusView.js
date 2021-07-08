308f3ac744aa421628219188ab0c3e7dcaa4aa03
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-08 01:01:46 -0400

    Campus view styled to include extra properties

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -2,13 +2,19 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { EditCampusContainer } from '../containers';
+import '../css/CampusView.css';
 
 const CampusView = (props) => {
   const {campus} = props;
   return (
-    <div>      
-      <h1>{campus.name}</h1>
-      <p>{campus.description}</p>
+    <div className="campus-view">
+      <img className="campus-img" src={campus.imageURL} />
+      <div className="campus-text">
+        <h1>{campus.name}</h1>
+        <h3>{campus.address}</h3>
+        <p>{campus.description}</p>
+      </div>
+      <p>Enrolled Students:</p>
       <ul>
       {
         campus.students.length
@@ -16,23 +22,29 @@ const CampusView = (props) => {
           campus.students.map( student => {
           let name = student.firstname + " " + student.lastname;
           return (
-            <li key={student.id}>{name}</li>
+            <li key={student.id}>
+              <Link to={"/student/" + student.id}>
+                {name}
+              </Link>
+            </li>
           );})
         :
-          <p>No Student Enrolled</p>
+          <li>No Students Enrolled</li>
       }
       </ul>
+      <div className="button-menu">
+        <Button onClick={props.showEditClick} variant="contained" color="primary">
+          Edit Campus
+        </Button>
+        <Button onClick={props.handleClick} variant="contained" color="primary">
+          Delete Campus
+        </Button>
+      </div>
       {
         props.showEdit
         ? <EditCampusContainer refresh={props.refresh} showEdit={props.showEditClick} campus={campus}/>
         : <></>
       }
-      <Button onClick={props.showEditClick} variant="contained" color="primary">
-        Edit Campus
-      </Button>
-      <Button onClick={props.handleClick} variant="contained" color="primary">
-        Delete Campus
-      </Button>
     </div>
   );
 };
