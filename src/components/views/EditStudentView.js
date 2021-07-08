10ae9d0236f8be2acb81dc562daf17c446ab627a
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-07-08 04:07:01 -0400

    Styled Edit Student

--- src/components/views/EditStudentView.js
+++ src/components/views/EditStudentView.js
@@ -1,19 +1,65 @@
 import Button from '@material-ui/core/Button';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import TextField from '@material-ui/core/TextField';
 import RadioGroup from '@material-ui/core/RadioGroup';
+import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
-import { AllCampusesContainer } from '../containers';
+import "../css/EditStudentView.css"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      margin: "15px"
+    }
+}));
 
 const EditStudentView = (props) => {
-    console.log(props.allCampuses);
+    const classes = useStyles();
     return (
-        <div>
+        <div className="edit-student">
             <form id="edit-student-form" onSubmit={props.handleSubmit}>
-            <div className="form-entry">
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="First Name" name="firstname" 
+                    onChange={props.handleChange} value={props.student.firstname} 
+                    fullWidth required/>
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="Last Name" name="lastname" 
+                    onChange={props.handleChange} value={props.student.lastname}
+                    fullWidth required/>
+                <TextField 
+                    className={classes.root} 
+                    variant="outlined" label="Image URL" name="imageURL"
+                    onChange={props.handleChange} value={props.student.imageURL}
+                    fullWidth />
+                <TextField
+                    className={classes.root} 
+                    type="number" InputProps={{inputProps: {min: 0, max: 4.0, step: 0.01}}}
+                    variant="outlined" label="GPA" name="gpa"
+                    onChange={props.handleChange} value={props.student.gpa}
+                    fullWidth/>
+                <FormLabel component="legend">Campus</FormLabel>
+                <RadioGroup name="campusId" value={props.student.campusId === null ? "" : props.student.campusId} onChange={props.handleChange}>
+                    {
+                        props.allCampuses.map((campus) => {
+                            return(
+                                <FormControlLabel 
+                                    checked={parseInt(props.student.campusId) === campus.id}
+                                    key={campus.id}
+                                    value={campus.id}
+                                    control={<Radio />}
+                                    label={campus.name} />)
+                        })
+                    }
+                    <FormControlLabel value="" control={<Radio />} label="None" />
+                </RadioGroup>
+                <Button 
+                    className={classes.root} type="submit"
+                    variant="contained" color="primary">Submit</Button>
+            </form>
+            {/* <div className="form-entry">
                 <label htmlFor="firstname">First Name: </label>
                 <input type="text" name="firstname" onChange={props.handleChange} value={props.student.firstname} required />
             </div>
@@ -21,21 +67,8 @@ const EditStudentView = (props) => {
                 <label htmlFor="lastname">Last Name: </label>
                 <input type="text" name="lastname" onChange={props.handleChange} value={props.student.lastname} required />
             </div>
-            <RadioGroup aria-label="campus" name="campusId" value={props.student.campusId === null ? "" : props.student.campusId} onChange={props.handleChange}>
-                {
-                    props.allCampuses.map((campus) => {
-                        return(<FormControlLabel 
-                            checked={parseInt(props.student.campusId) === campus.id}
-                            key={campus.id}
-                            value={campus.id}
-                            control={<Radio />}
-                            label={campus.name} />)
-                    })
-                }
-                <FormControlLabel value="" control={<Radio />} label="None" />
-            </RadioGroup>
             <Button type="submit" id="edit-submit" variant="contained" color="primary">Submit</Button>
-            </form>
+            </form> */}
         </div>
     );
   };
